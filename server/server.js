6e1ebdcef4badcf6076ba139d904b0421845ec8f
Diana <d.abulebbeh@mangafruit.com>
2019-02-17 14:32:01 +0200

    Test Post/todos routes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ var { Todo } = require("./models/todo");
 var { User } = require("./models/user");
 var app = express();
 app.use(bodyParser.json());
+
 app.post("/todos", (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -23,3 +24,4 @@ app.post("/todos", (req, res) => {
 app.listen(3000, () => {
   console.log("Started o posr 3000");
 });
+module.exports = { app };
